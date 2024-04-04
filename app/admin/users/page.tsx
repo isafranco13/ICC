950459785ac7b8d20b0b9402cb07eda5d109ffda
starthare <adriana26.mn@gmail.com>
2024-04-04 00:42:11 -0600

    agregar sección terapeutas en lista de usuarios

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,14 +1,28 @@
 "use client"
 import NavBarAdmin from '@/components/NavBarAdmin';
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function Home() {
+    const router = useRouter();
+    //USUARIOS
     const [usuarios, setUsuarios] = useState<{ _id: string, nombre: string, correo: string, apellido: string }[]>([]);
 
     useEffect(() => {
         fetch('/api/usuarios')
             .then(response => response.json())
             .then(data => setUsuarios(data.usuarios));
+    }, []);
+    
+    //TERAPEUTAS
+    const [terapeutas, setTerapeutas] = useState<{ _id: string, nombre: string, apellidoPaterno: string, apellidoMaterno: string, celular: string, correo: string }[]>([]);
+
+    useEffect(() => {
+        fetch('/api/terapeutas')
+            .then(response => response.json())
+            .then(data => setTerapeutas(data.terapeutas));
     }, []);
 
     return (
@@ -20,32 +34,71 @@ export default function Home() {
                         <NavBarAdmin />
                     </div>
 
-                    <div className='text'>
-                        <p className='tituloAdmin'><strong>Usuarios</strong></p>
-                    </div>
+                    <div style={{ display: 'inline-block', width: '100%', marginLeft: '2%' , marginRight: '2%'}}>
+                        <div className='text'>
+                            <p className='tituloAdmin'><strong>Usuarios</strong></p>
+                        </div>
 
-                    <div className='text'>
-                        <h1>Cantidad de Usuarios: {usuarios.length}</h1><br />
-                        <table className='text-center'>
-                            <thead>
-                                <tr>
-                                    <th className='pr-10'>Nombre</th>
-                                    <th className='pr-10'>Apellido</th>
-                                    <th>Correo</th>
-                                    {/* Agregar más columnas según la estructura de los usuarios */}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {usuarios.map(usuario => (
-                                    <tr key={usuario._id}>
-                                        <td className='pr-10'>{usuario.nombre}</td>
-                                        <td className='pr-10'>{usuario.apellido}</td>
-                                        <td>{usuario.correo}</td>
-                                        {/* Agregar más celdas según la estructura de los usuarios */}
+                        <div className='text bg-[#FFEBA9] rounded-3xl justify-center items-center'>
+                            <table className='text-center mx-auto w-full'>
+                                <thead className='text-[#F5FFFC] bg-pink-400'>
+                                    <tr className='px-5'>
+                                        <th className='rounded-t-xl'>Nombre</th>
+                                        <th className='rounded-t-xl'>Apellido</th>
+                                        <th className='rounded-t-xl'>Correo</th>
+                                        <th className='rounded-t-xl'>Action</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody className='text-green-500 bg-green-50'>
+                                    {usuarios.map(usuario => (
+                                        <tr key={usuario._id} className='px-5 py-1'>
+                                            <td className='py-1.5'>{usuario.nombre}</td>
+                                            <td className='py-1.5'>{usuario.apellido}</td>
+                                            <td className='py-1.5'>{usuario.correo}</td>
+                                            <td className='py-1.5 flex text-2xl justify-center items-center'>
+                                                <button className='text-green-400 cursor-pointer mr-1'><FaRegEdit /></button>
+                                                <button className='text-red-400 cursor-pointer'><RiDeleteBin6Line /></button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {/* TERAPEUTAS */}
+                        <div className='text'>
+                            <p className='tituloAdmin'><strong>Terapeutas</strong></p>
+                        </div>
+
+                        <div className='text bg-[#FFEBA9] rounded-3xl justify-center items-center'>
+                            <table className='text-center mx-auto w-full'>
+                                <thead className='text-[#F5FFFC] bg-pink-400'>
+                                    <tr className='px-5'>
+                                        <th className='rounded-t-xl'>Nombre</th>
+                                        <th className='rounded-t-xl'>Apellido Paterno</th>
+                                        <th className='rounded-t-xl'>Apellido Materno</th>
+                                        <th className='rounded-t-xl'>Celular</th>
+                                        <th className='rounded-t-xl'>Correo</th>
+                                        <th className='rounded-t-xl'>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='text-green-500 bg-green-50'>
+                                    {terapeutas.map(terapeuta => (
+                                        <tr key={terapeuta._id} className='px-5 py-1'>
+                                            <td className='py-1.5'>{terapeuta.nombre}</td>
+                                            <td className='py-1.5'>{terapeuta.apellidoPaterno}</td>
+                                            <td className='py-1.5'>{terapeuta.apellidoMaterno}</td>
+                                            <td className='py-1.5'>{terapeuta.celular}</td>
+                                            <td className='py-1.5'>{terapeuta.correo}</td>
+                                            <td className='py-1.5 flex text-2xl justify-center items-center'>
+                                                <button className='text-green-400 cursor-pointer mr-1'><FaRegEdit /></button>
+                                                <button className='text-red-400 cursor-pointer'><RiDeleteBin6Line /></button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
