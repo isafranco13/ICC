c11a86dc4403b52b94ce363a0fa1dd5e36d7fce6
starthare <adriana26.mn@gmail.com>
2024-04-05 02:35:43 -0600

    agregar componente Alert Dialog para confirmar antes de eliminar un usuario

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -4,19 +4,24 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import CustomAlertDialog from '@/components/CustomAlertDialog';
 
 export default function Home() {
     const router = useRouter();
+    const [selectedItemId, setSelectedItemId] = useState('');
+    const [isAlertDialogOpen, setIsAlertDialogOpen] = useState(false);
+    const [alertDialogTitle, setAlertDialogTitle] = useState('');
+    const [alertDialogBody, setAlertDialogBody] = useState('');
     //USUARIOS
     const [usuarios, setUsuarios] = useState<{ _id: string, nombre: string, correo: string, apellido: string }[]>([]);
 
-    useEffect(() => {
+    useEffect(() => { //cargar usuarios
         fetch('/api/usuarios')
             .then(response => response.json())
             .then(data => setUsuarios(data.usuarios));
     }, []);
 
-    const handleDeleteUser = async ( id: string ) => {
+    /*const handleDeleteUser = async ( id: string ) => {
         const res = await fetch(`/api/usuarios?id=${id}`, {
             method: 'DELETE',
         });
@@ -24,7 +29,14 @@ export default function Home() {
             setUsuarios(usuarios.filter(usuario => usuario._id !== id));
             router.refresh();
         }
-    }
+    }*/
+
+    const handleDeleteUserConfirmation = async (userId: string) => {
+        setSelectedItemId(userId);
+        setAlertDialogTitle('Eliminar Usuario');
+        setAlertDialogBody('¿Estás seguro de que deseas eliminar este usuario? No podrás deshacer esta acción después.');
+        setIsAlertDialogOpen(true);
+    };
     
     //TERAPEUTAS
     const [terapeutas, setTerapeutas] = useState<{ _id: string, nombre: string, apellidoPaterno: string, apellidoMaterno: string, celular: string, correo: string }[]>([]);
@@ -35,7 +47,7 @@ export default function Home() {
             .then(data => setTerapeutas(data.terapeutas));
     }, []);
 
-    const handleDeleteTerap = async ( id: string ) => {
+    /*const handleDeleteTerap = async ( id: string ) => {
         const res = await fetch(`/api/terapeutas?id=${id}`, {
             method: 'DELETE',
         });
@@ -43,18 +55,52 @@ export default function Home() {
             setTerapeutas(terapeutas.filter(terapeuta => terapeuta._id !== id));
             router.refresh();
         }
-    }
+    }*/
+
+    const handleDeleteTerapConfirmation = async (terapId: string) => {
+        setSelectedItemId(terapId);
+        setAlertDialogTitle('Eliminar Terapeuta');
+        setAlertDialogBody('¿Estás seguro de que deseas eliminar este terapeuta? No podrás deshacer esta acción después.');
+        setIsAlertDialogOpen(true);
+    };
+
+    //Función para eliminar después de la confirmación de la eliminación de un usuario o terapeuta
+    const handleDelete = async () => {
+        // Verificar si el selectedItem es un usuario o un terapeuta
+        const isUser = usuarios.some(usuario => usuario._id === selectedItemId);
+        if (isUser) {
+            // Es un usuario, realizar la eliminación
+            const res = await fetch(`/api/usuarios?id=${selectedItemId}`, {
+                method: 'DELETE',
+            });
+            if (res.ok) {
+                setUsuarios(usuarios.filter(usuario => usuario._id !== selectedItemId));
+                router.refresh();
+            }
+        } else {
+            // Es un terapeuta, realizar la eliminación
+            const res = await fetch(`/api/terapeutas?id=${selectedItemId}`, {
+                method: 'DELETE',
+            });
+            if (res.ok) {
+                setTerapeutas(terapeutas.filter(terapeuta => terapeuta._id !== selectedItemId));
+                router.refresh();
+            }
+        }
+        setIsAlertDialogOpen(false);
+        setSelectedItemId('');
+    };
 
     return (
         
         <main className="overflow-hidden">
             <div className="contenedor">
-                <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', backgroundColor: '#F5FFFC' }}>
                     <div>
                         <NavBarAdmin />
                     </div>
 
-                    <div style={{ display: 'inline-block', width: '100%', marginLeft: '2%' , marginRight: '2%'}}>
+                    <div style={{ display: 'inline-block', width: '100%', marginLeft: '2%' , marginRight: '2%' }}>
                         <div className='text'>
                             <p className='tituloAdmin'><strong>Usuarios</strong></p>
                         </div>
@@ -77,7 +123,7 @@ export default function Home() {
                                             <td className='py-1.5'>{usuario.correo}</td>
                                             <td className='py-1.5 flex text-2xl justify-center items-center'>
                                                 <button className='text-green-400 cursor-pointer mr-1'><FaRegEdit /></button>
-                                                <button className='text-red-400 cursor-pointer' onClick={() => handleDeleteUser(usuario._id)}><RiDeleteBin6Line /></button>
+                                                <button className='text-red-400 cursor-pointer' onClick={() => handleDeleteUserConfirmation(usuario._id)}><RiDeleteBin6Line /></button>
                                             </td>
                                         </tr>
                                     ))}
@@ -112,12 +158,23 @@ export default function Home() {
                                             <td className='py-1.5'>{terapeuta.correo}</td>
                                             <td className='py-1.5 flex text-2xl justify-center items-center'>
                                                 <button className='text-green-400 cursor-pointer mr-1'><FaRegEdit /></button>
-                                                <button className='text-red-400 cursor-pointer' onClick={() => handleDeleteTerap(terapeuta._id)}><RiDeleteBin6Line /></button>
+                                                <button className='text-red-400 cursor-pointer' onClick={() => handleDeleteTerapConfirmation(terapeuta._id)}><RiDeleteBin6Line /></button>
                                             </td>
                                         </tr>
                                     ))}
+                                    
                                 </tbody>
                             </table>
+                            {/* Componente AlertDialog */}
+                            <CustomAlertDialog
+                                        isOpen={isAlertDialogOpen}
+                                        headerText={alertDialogTitle}
+                                        bodyText={alertDialogBody}
+                                        cancelText='Cancelar'
+                                        confirmText='Eliminar'
+                                        colorScheme='red'
+                                        onConfirm={handleDelete}
+                                    />
                         </div>
                     </div>
                 </div>
