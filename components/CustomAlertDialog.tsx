475a030ad3c3aef962a55be1f17d57ebeb8d0ba0
starthare <adriana26.mn@gmail.com>
2024-04-05 02:34:55 -0600

    agregar componente Alert Dialog

---
+++ components/CustomAlertDialog.tsx
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react';
+import { ChakraProvider, Button, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, 
+    AlertDialogContent, AlertDialogOverlay, useDisclosure } from '@chakra-ui/react';
+
+interface CustomAlertDialogProps {
+    isOpen: boolean;
+    headerText: string;
+    bodyText: string;
+    cancelText: string;
+    confirmText: string;
+    colorScheme: string;
+    onConfirm: () => void;
+}
+
+const CustomAlertDialog: React.FC<CustomAlertDialogProps> = ({ isOpen, headerText, bodyText, cancelText, confirmText, colorScheme, onConfirm  }) => {
+    const { onClose } = useDisclosure()
+    const cancelRef = useRef<HTMLButtonElement>(null);
+
+    return (
+        <>
+                <ChakraProvider>
+                    <AlertDialog
+                        isOpen={isOpen}
+                        leastDestructiveRef={cancelRef}
+                        onClose={onClose}
+                        isCentered
+                    >
+                        <AlertDialogOverlay>
+                            <AlertDialogContent>
+                            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                                {headerText}
+                            </AlertDialogHeader>
+                            <AlertDialogBody>
+                                {bodyText}
+                            </AlertDialogBody>
+                            <AlertDialogFooter>
+                                <Button ref={cancelRef} onClick={onClose}>
+                                {cancelText}
+                                </Button>
+                                <Button colorScheme={colorScheme} onClick={onConfirm} ml={3}>
+                                {confirmText}
+                                </Button>
+                            </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialogOverlay>
+                    </AlertDialog>
+                </ChakraProvider>
+        </>
+    );
+};
+
+export default CustomAlertDialog;
