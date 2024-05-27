a5bf04f248673d2563fdd81ae3fbe38990e716a2
starthare <adriana26.mn@gmail.com>
2024-05-27 11:11:37 -0600

    agregar al metodo post que se cree el perfil del terapeuta al crear un terapeuta

--- app/api/terapeutas/route.js
+++ app/api/terapeutas/route.js
@@ -1,22 +1,32 @@
 import { connectDB } from "@/libs/mongodb";
-import  Terapeutas from "@/models/terapeutas";
+import  Usuarios from "@/models/usuarios";
+import PerfilTerapeutas from "@/models/perfil_terap";
 import { NextResponse } from "next/server";
 
 //Función GET para obtener todos los terapeutas
 export async function GET() {
     await connectDB();
-    const terapeutas = await Terapeutas.find({});
+    const terapeutas = await Usuarios.find({ role: "terapeuta" });
     return NextResponse.json({ terapeutas });
 }
 
 export async function POST(request) {
     await connectDB();
     const data = await request.json();
-    const terapeutaExists = await Terapeutas.findOne({ correo: data.correo });
+    const terapeutaExists = await Usuarios.findOne({ correo: data.correo });
 
     if (!terapeutaExists) {
-        const terapeuta = await Terapeutas.create(data); // Crea un nuevo terapeuta
-        return NextResponse.json({ terapeuta }); // Devuelve la respuesta con el terapeuta creado
+        const nuevoTerapeuta = await Usuarios.create({ ...data, role: "terapeuta" }); // Crea un nuevo terapeuta
+        // Crear el perfil del terapeuta con los datos proporcionados
+        const perfilTerapeuta = await PerfilTerapeutas.create({
+            usuarioId: nuevoTerapeuta._id,
+            nombre: data.nombre,
+            apellidoPaterno: data.apellidoPaterno,
+            apellidoMaterno: data.apellidoMaterno,
+            celular: data.celular,
+            // Los campos no proporcionados se dejarán vacíos o con valores predeterminados
+        });
+        return NextResponse.json({ terapeuta: nuevoTerapeuta, perfil: perfilTerapeuta }); // Devuelve la respuesta con el terapeuta y perfil creado
     } else {
         return NextResponse.json({ message: "Terapeuta ya existe" });
     }
@@ -25,6 +35,6 @@ export async function POST(request) {
 export async function DELETE(request) {
     const id = request.nextUrl.searchParams.get("id");
     await connectDB();
-    await Terapeutas.findByIdAndDelete(id);
+    await Usuarios.findByIdAndDelete(id);
     return NextResponse.json({ message: "Terapeuta eliminado" });
 }
