a88d9814936ac3654377d762aee3b85fbf7a1e62
starthare <adriana26.mn@gmail.com>
2024-05-26 21:44:07 -0600

    arreglar importe de los componentes NavBar, Footer e Intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { CustomButton, Intro } from "@/components";
+import Intro from "@/components/Intro";
 import Catalog from "@/components/Catalog";
 import QBP from "@/components/QBP";
 import Image from "next/image";
 import Link from "next/link";
-import { Footer, Navbar } from "@/components";
-
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
