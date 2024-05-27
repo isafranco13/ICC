91a9236c5aed71439f376d59c48ac29d6c07a53c
starthare <adriana26.mn@gmail.com>
2024-05-27 11:23:26 -0600

    obtener y mostrar todos los terapeutas disponibles con nombre y apellido

--- app/usuario/page.tsx
+++ app/usuario/page.tsx
@@ -1,24 +1,32 @@
-import { Footer} from "@/components"; {/*{ Footer, NavbarSignOut } */}
+"use client"
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import Image from 'next/image';
 import NavbarUsuario from "@/components/NavbarUsuario";
 import type { User } from "next-auth"
-
+import { useEffect, useState } from 'react';
 
 type Props = {
     user: User,
 }
   
-const Dashboard = async () => { // async ({user}: Props)  -> NO BORRAR ESTA EN PRUEBA
-    const session = await getServerSession();
+export default function Dashboard() { // async ({user}: Props)  -> NO BORRAR ESTA EN PRUEBA
+    /*const session = await getServerSession();
     if(session?.user.rol !== "terapeuta" && !session){
         redirect("/signin");
     }
     
     if (!session) {
       redirect("/signin");
-    }
+    }*/
+
+    const [terapeutas, setTerapeutas] = useState<{ _id: string, nombre: string, apellido: string }[]>([]);
+
+    useEffect(() => {
+        fetch('/api/terapeutas')
+            .then(response => response.json())
+            .then(data => setTerapeutas(data.terapeutas));
+    }, []);
     
     return (
         <>                            <NavbarUsuario/>
@@ -29,87 +37,83 @@ const Dashboard = async () => { // async ({user}: Props)  -> NO BORRAR ESTA EN P
                         </div>
     */} 
                         <div className="section-1 h-max">
-                            <h1 className="font-bold text-2xl">Terapeutas</h1>
-                                <div className="therapistAppointment">
-                                    <div className="flex items-center"> 
-                                        <Image
-                                        src="/user.png"
-                                        alt="perfil"
-                                        width={66} //97
-                                        height={66} //96
-                                        className=" object-contain rounded-full bg-white " 
-                                        />
-                                    </div>
-                                    <div className="ms-[15px] flex flex-col">
-                                        <h2 className="subtitleHome">Psic. Lorena</h2>
-                                        <div className="flex flex-row">
-                                            <Image
-                                            src="/reloj.png"
-                                            alt="reloj"
-                                            width={20} 
-                                            height={20} 
-                                            className=" object-contain" 
-                                            />
-                                            <p className="ms-[10px] font-medium text-[20px]">8:00-19:00</p>
-                                        </div>
+                <h1 className="font-bold text-2xl">Terapeutas</h1>
+                {terapeutas.map((terapeuta) => (
+                    <div key={terapeuta._id} className="therapistAppointment">
+                        <div className="flex items-center">
+                            <Image
+                                src="/user.png"
+                                alt="perfil"
+                                width={66}
+                                height={66}
+                                className=" object-contain rounded-full bg-white "
+                            />
+                        </div>
+                        <div className="ms-[15px] flex flex-col">
+                            <h2 className="subtitleHome">{`Psic. ${terapeuta.nombre} ${terapeuta.apellido}`}</h2>
+                            <div className="flex flex-row">
+                                <Image
+                                    src="/reloj.png"
+                                    alt="reloj"
+                                    width={20}
+                                    height={20}
+                                    className=" object-contain"
+                                />
+                                <p className="ms-[10px] font-medium text-[20px]">8:00-19:00</p>
+                            </div>
 
-                                        <div className="flex flex-row">
-                                            <Image
-                                            src="/calendario.png"
-                                            alt="calendario"
-                                            width={20} 
-                                            height={20} 
-                                            className=" object-contain" 
-                                            />
-                                            <a href="#"><p className="ms-[10px] font-medium text-[20px] hover:text-[#05814E]">Ver agenda</p></a>
-                                        </div>
-                                    </div>
+                            <div className="flex flex-row">
+                                <Image
+                                    src="/calendario.png"
+                                    alt="calendario"
+                                    width={20}
+                                    height={20}
+                                    className=" object-contain"
+                                />
+                                <a href="#"><p className="ms-[10px] font-medium text-[20px] hover:text-[#05814E]">Ver agenda</p></a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="section-2 h-max">
+                <h1 className="font-bold text-2xl">Próxima Cita</h1>
+                <div className="therapistAppointment items-center">
+                    <div className="flex bg-white rounded-[29px] w-[70px] h-[70px] relative">
+                        <h3 className="pl-[10px] font-semibold text-[28px] absolute top-0 left-0">Jun</h3>
+                        <h3 className="pl-[20px] font-semibold text-[28px] absolute bottom-0 left-0">10</h3>
+                    </div>
+                    <div className="ms-[15px] flex flex-col">
+                        <h2 className="subtitleHome">Psic. Lorena</h2>
+                        <div className="flex items-center">
+                            <div className="flex items-center">
+                                <div className="flex flex-row">
+                                    <Image
+                                        src="/reloj.png"
+                                        alt="reloj"
+                                        width={20}
+                                        height={20}
+                                        className=" object-contain mr-2"
+                                    />
+                                    <p className="font-medium text-[20px]">10:00am</p>
                                 </div>
-                            </div>{/*fin seccion 1*/}
 
-
-
-
-                            <div className="section-2 h-max">
-                            <h1 className="font-bold text-2xl">Próxima Cita</h1>
-                                <div className="therapistAppointment items-center">
-                                    <div className="flex bg-white rounded-[29px] w-[70px] h-[70px] relative">
-                                        <h3 className="pl-[10px] font-semibold text-[28px] absolute top-0 left-0">Jun</h3>
-                                        <h3 className="pl-[20px] font-semibold text-[28px] absolute bottom-0 left-0">10</h3>
-                                    </div>
-                                    <div className="ms-[15px] flex flex-col">
-                                        <h2 className="subtitleHome">Psic. Lorena</h2>
-                                        <div className="flex items-center">
-                                            <div className="flex items-center">
-                                                <div className="flex flex-row">
-                                                    <Image
-                                                    src="/reloj.png"
-                                                    alt="reloj"
-                                                    width={20} 
-                                                    height={20} 
-                                                    className=" object-contain mr-2" 
-                                                    />
-                                                    <p className="font-medium text-[20px]">10:00am</p>
-                                                </div>
-
-                                                <div className="flex items-center">
-                                                    <Image
-                                                    src="/ubicacion.png"
-                                                    alt="ubi"
-                                                    width={20} 
-                                                    height={20} 
-                                                    className=" object-contain mr-2 ms-[40px]" 
-                                                    />
-                                                    <a href="#"><p className="font-medium text-[20px] hover:text-[#05814E]">Ver ubicación</p></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div className="flex items-center">
+                                    <Image
+                                        src="/ubicacion.png"
+                                        alt="ubi"
+                                        width={20}
+                                        height={20}
+                                        className=" object-contain mr-2 ms-[40px]"
+                                    />
+                                    <a href="#"><p className="font-medium text-[20px] hover:text-[#05814E]">Ver ubicación</p></a>
                                 </div>
-                            </div>{/*fin seccion 2*/}
-                   {/* </div>*/}
-                </>
-);
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
-
-export default Dashboard;
