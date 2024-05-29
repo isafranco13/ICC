7e326c941d027fa22bb476de0233c7e4f331d976
starthare <adriana26.mn@gmail.com>
2024-05-28 22:10:51 -0600

    metodos para obtener fechas y horas disponibles y agendar citas

--- app/api/citas/route.js
+++ app/api/citas/route.js
@@ -1,35 +1,54 @@
-import { connectDB } from "@/libs/mongodb";
-import Cita from "@/models/citas";
+import clientPromise from "@/libs/mongodb";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
+import Cita from "@/models/citas";
 
 // Obtener horas disponibles para una fecha específica
 export async function GET(request) {
-  await connectDB();
+  const client = await clientPromise;
+  const db = client.db();
   const fecha = new Date(request.nextUrl.searchParams.get("fecha"));
-  const citas = await Cita.find({ fecha });
+  
+  try {
+    const citas = await db.collection("citas").find({ fecha }).toArray();
 
-  const horasReservadas = citas.map(cita => cita.hora);
-  const horasDisponibles = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"].filter(
-    hora => !horasReservadas.includes(hora)
-  );
+    const horasReservadas = citas.map(cita => cita.hora);
+    const horasDisponibles = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"].filter(
+      hora => !horasReservadas.includes(hora)
+    );
 
-  return NextResponse.json(horasDisponibles);
+    return NextResponse.json(horasDisponibles);
+  } catch (error) {
+    return NextResponse.error(error);
+  }
 }
 
 // Agendar una cita
 export async function POST(request) {
-  await connectDB();
-  const data = await request.json();
-  const { fecha, hora, usuarioId, terapeutaId } = data;
+  const client = await clientPromise;
+  const db = client.db();
+  
+  try {
+    const { fecha, hora, nombre, apellidoPaterno, apellidoMaterno, celular, usuarioId, terapeutaId } = await request.json();
+  
+    const data = new Cita({
+      fecha: new Date(fecha), // Asegúrate de que la fecha se guarde correctamente como Date
+      hora,
+      nombre,
+      apellidoPaterno,
+      apellidoMaterno,
+      celular,
+      terapeutaId: new ObjectId(terapeutaId),
+    });
 
-  const citaExistente = await Cita.findOne({ fecha, hora, terapeutaId });
+    if (usuarioId) {
+      data.usuarioId = new ObjectId(usuarioId);
+    }
 
-  if (citaExistente) {
-    return NextResponse.json({ message: "Hora no disponible" });
+    await db.collection("citas").insertOne(data);
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.error(error);
   }
-
-  const nuevaCita = new Cita(data);
-  await nuevaCita.save();
-
-  return NextResponse.json(nuevaCita);
 }
